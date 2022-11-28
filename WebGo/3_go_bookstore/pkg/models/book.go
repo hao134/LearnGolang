03b85d61b46a7a932868a6df73c621f2191b2cfa
hao134 <hao134@github.com>
2022-11-28 20:27:48 +0800

    simple bank

---
+++ WebGo/3_go_bookstore/pkg/models/book.go
@@ -0,0 +1,6 @@
+package models
+
+import {
+	"github.com/jinzhu/gorm"
+	"github.com/hao134/go-bookstore/pkg/config"
+}
---
+++ WebGo/3_go_bookstore/readme.md
@@ -0,0 +1,5 @@
+![](https://i.imgur.com/aRGYd0r.png)
+
+## Routes
+
+![](https://i.imgur.com/2h0r2x1.png)
